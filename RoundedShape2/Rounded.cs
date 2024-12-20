b083066851f7197c9865ea24926018de98b2f49d
ataliesavista <ataliesavista02@gmail.com>
2024-12-20 19:05:07 +0700

    Add project files.

---
+++ RoundedShape2/Rounded.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace RoundedShape2
+{
+    //mengatur sudut components supaya lengkung
+    public class Rounded
+    {
+        //control => jenis components yang akan dibuat lengkung
+        //radius => besar kelengkungan yang diinginkan
+        public static void SetRounded(Control control, int radius)
+        {
+            //AddArch mengatur kelengkungan untuk tiap sudut components
+            if (control == null || radius <= 0) return;
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(control.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(control.Width - radius, control.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, control.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+
+            control.Region = new Region(path);
+        }
+    }
+}
